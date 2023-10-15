1fc8433f4bc8c17f627d8af1c142edb1d39e4bf5
testing doonly <106380029+DPLakmal@users.noreply.github.com>
2023-10-15 19:40:42 +0530

    Create BookStore frontend

---
+++ bookstore-frontend/src/pages/CreateBooks.jsx
@@ -0,0 +1,102 @@
+import React, { useState } from "react";
+import BackButton from "../components/BackButton";
+import Spinner from "../components/Spinner";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { useSnackbar } from "notistack";
+
+const CreateBooks = () => {
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const [publishYear, setPublishYear] = useState("");
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const { enqueueSnackbar } = useSnackbar();
+
+  const handleSaveBook = () => {
+    const data = {
+      title,
+      author,
+      publishYear,
+    };
+    setLoading(true);
+    axios
+      .post("http://localhost:5555/books", data)
+      .then(() => {
+        setLoading(false);
+        enqueueSnackbar("Book Created successfully", { variant: "success" });
+        navigate("/");
+      })
+      .catch((error) => {
+        setLoading(false);
+        // alert('An error happened. Please Chack console');
+        enqueueSnackbar("Error", { variant: "error" });
+        console.log(error);
+      });
+  };
+
+  return (
+    <div className="p-4">
+      <BackButton />
+      <div>
+        {/* <!-- Image gallery --> */}
+        <div class="mx-auto mt-6 max-w-2xl sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-1 lg:gap-x-8 lg:px-8">
+         
+          {loading ? <Spinner /> : ""}
+
+          <div className="flex flex-col  border-2 border-sky-400 rounded-xl w-[900px] p-4 mx-auto">
+          <h1 className="text-3xl ml-8 m-3">Create Book</h1>
+            <div className=" columns-2 mx-4 mt-1">
+              <div className="">
+                <div className="my-4">
+                  <label className="text-xl mr-4 text-gray-500">Title</label>
+                  <input
+                    type="text"
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                    className="border-2 border-gray-500 px-4 py-2 w-full"
+                  />
+                </div>
+                <div className="my-4">
+                  <label className="text-xl mr-4 text-gray-500">Author</label>
+                  <input
+                    type="text"
+                    value={author}
+                    onChange={(e) => setAuthor(e.target.value)}
+                    className="border-2 border-gray-500 px-4 py-2  w-full "
+                  />
+                </div>
+                <div className="my-4">
+                  <label className="text-xl mr-4 text-gray-500">
+                    Publish Year
+                  </label>
+                  <input
+                    type="number"
+                    value={publishYear}
+                    onChange={(e) => setPublishYear(e.target.value)}
+                    className="border-2 border-gray-500 px-4 py-2  w-full "
+                  />
+                </div>
+                <button className="px-6 py-3 rounded-md bg-lime-500 m-8" onClick={handleSaveBook}>
+                   Save 
+                </button>
+              </div>
+
+              <div class="aspect-h-2 aspect-w-1  hidden overflow-hidden rounded-lg sm:block">
+                <img
+                  src="https://tailwindui.com/img/ecommerce-images/product-page-02-secondary-product-shot.jpg"
+                  alt="Two each of gray, white, and black shirts laying flat."
+                  class="h-full w-full object-cover object-center"
+                />
+              </div>
+
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CreateBooks;
---
+++ bookstore-frontend/src/pages/Home.jsx
@@ -0,0 +1,81 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Spinner from "../components/Spinner";
+import { Link } from "react-router-dom";
+import { AiOutlineEdit } from "react-icons/ai";
+import { BsInfoCircle } from "react-icons/bs";
+import { MdOutlineAddBox, MdOutlineDelete } from "react-icons/md";
+import BooksTable from "../components/home/BooksTable";
+import BooksCard from "../components/home/BooksCard";
+
+const Home = () => {
+  const [books, setBooks] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [showType, setShowType] = useState("table");
+
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get("http://localhost:5555/books")
+      .then((response) => {
+        setBooks(response.data.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+        setLoading(false);
+      });
+  }, []);
+
+  return (
+    <div class="min-h-screen">
+      
+      <header class="bg-gray-600 shadow  ">
+        <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8 ">
+          <div className="columns-2">
+            <h1 class="text-3xl font-bold tracking-tight text-gray-900">
+              {" "}
+              Books List
+            </h1>
+            <div className="flex justify-center items-center gap-x-4">
+              <button
+                className="bg-gray-500  hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+                onClick={() => setShowType("table")}
+              >
+                Table
+              </button>
+              <button
+                className="bg-gray-500 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+                onClick={() => setShowType("card")}
+              >
+                Card
+              </button>
+              <div className="flex justify-between items-center">
+              <Link to="/books/create">
+                <MdOutlineAddBox className="text-sky-800 text-4xl" />
+              </Link>
+            </div>
+            </div>
+          </div>
+        </div>
+      </header>
+      <main >
+        <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+          {/* <!-- Your content --> */}
+          <div className="p-4 ">
+         
+            {loading ? (
+              <Spinner />
+            ) : showType === "table" ? (
+              <BooksTable books={books} />
+            ) : (
+              <BooksCard books={books} />
+            )}
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default Home;
