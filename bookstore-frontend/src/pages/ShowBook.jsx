e4479224cce79d163119cbc3a2ebc433effd6591
testboy123234 <148220701+testboy123234@users.noreply.github.com>
2023-10-30 09:38:39 -0700

    Update ShowBook.jsx

--- bookstore-frontend/src/pages/ShowBook.jsx
+++ bookstore-frontend/src/pages/ShowBook.jsx
@@ -12,7 +12,7 @@ const ShowBook = () => {
   useEffect(() => {
     setLoading(true);
     axios
-      .get(`http://localhost:5555/books/${id}`)
+      .get(`https://books-store-api.vercel.app/api/v1/books/${id}`)
       .then((response) => {
         setBook(response.data);
         setLoading(false);
